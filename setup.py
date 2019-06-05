7c9cea77e77e9c192c3ba2cc6db38187197abc1a
Dmitry <killmaster00431@gmail.com>
2019-06-06 00:37:03 +0500

    Version 0.6.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tagging-autocomplete-new',
-    version='0.6',
+    version='0.6.1',
     description="Autocompletion for django-tagging. Rebuild for Django 2.2. Original by Ludwik Trammer.",
     long_description=markdown.clean(README),
     long_description_content_type="text/markdown",
@@ -28,7 +28,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Framework :: Django',
+        'Framework :: Django :: 2.2',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords=[
