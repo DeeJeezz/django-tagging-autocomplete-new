7add98af1f5f92448f08fe513d29ba84255af9ed
Dmitry <killmaster00431@gmail.com>
2019-06-05 22:16:44 +0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import find_packages, setup
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
+with open(os.path.join(os.path.dirname(__file__), 'LICENSE')) as lic:
+    LICENSE = lic.read()
+
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -17,6 +20,7 @@ setup(
     url='https://github.com/GrobIvanovich/django-tagging-autocomplete-new/',
     packages=find_packages(),
     include_package_data=True,
+    license=LICENSE,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -25,5 +29,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
-    ]
+    ],
+    keywords=['Django',
+              'Tags autocompletion',
+    ],
 )
