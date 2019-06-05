53f786ee63151977ed3272873f1b2fd9ada48f39
Dmitry <killmaster00431@gmail.com>
2019-06-05 23:40:15 +0500

    Deployed to PyPI.

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 import os
 from setuptools import find_packages, setup
+from readme_renderer import markdown
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
-
-with open(os.path.join(os.path.dirname(__file__), 'LICENSE')) as lic:
-    LICENSE = lic.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -13,14 +11,15 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-tagging-autocomplete-new',
     version='0.6',
-    description='Autocompletion for django-tagging. Rebuild for Django 2.2. Original by Ludwik Trammer.',
-    long_description=README,
+    description="Autocompletion for django-tagging. Rebuild for Django 2.2. Original by Ludwik Trammer.",
+    long_description=markdown.clean(README),
+    long_description_content_type="text/markdown",
     author='GrobIvanovich',
     author_email='killmaster00431@gmail.com',
     url='https://github.com/GrobIvanovich/django-tagging-autocomplete-new/',
     packages=find_packages(),
     include_package_data=True,
-    license=LICENSE,
+    license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -28,9 +27,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    keywords=['Django',
-              'Tags autocompletion',
+    keywords=[
+        'Django',
+        'Tags autocompletion',
     ],
 )
